eba694c6daf81080764d1027fe304e77ad5bc3c3
Choo Yi Jie <admin@chooyijie.com>
2025-11-16 21:40:51 +0800

    Remove all

--- client/src/components/examples/GestureCard.tsx
+++
@@ -1,5 +0,0 @@
-import GestureCard from '../GestureCard';
-
-export default function GestureCardExample() {
-  return <GestureCard gesture="Stop" confidence={0.92} />;
-}
--- client/src/components/examples/StatsPanel.tsx
+++
@@ -1,5 +0,0 @@
-import StatsPanel from '../StatsPanel';
-
-export default function StatsPanelExample() {
-  return <StatsPanel totalRecognitions={127} averageConfidence={0.87} recognitionRate={2.5} />;
-}
